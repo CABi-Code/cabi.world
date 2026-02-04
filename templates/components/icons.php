48ebd574f91f4ffe751b06ac8f7a6ac0b617d402
Ruslan <ruslanidolenko@gmail.com>
2026-02-04 16:55:44 +0100

    правки

--- templates/components/icons.php
+++ templates/components/icons.php
@@ -179,4 +179,9 @@
 		<circle cx="11" cy="11" r="8"/>
 		<line x1="21" y1="21" x2="16.65" y2="16.65"/>
 	</symbol>
+	
+	<symbol id="icon-copy" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+		<rect x="9" y="9" width="13" height="13" rx="2" ry="2"/>
+		<path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"/>
+	</symbol>
 </svg>
